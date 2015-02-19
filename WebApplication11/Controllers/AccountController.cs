802832583a2d202091ff715848943da0313b945b
Daan Le Duc <daanleduc@hotmail.com>
2015-02-19 15:30:00 +0100

    initial commit

---
+++ WebApplication11/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication11.Controllers
+{
+    [RoutePrefix("account")]
+    public class AccountController : ApiController
+    {
+        [HttpGet, Route("detail")]
+        public string AccountDetails()
+        {
+            return "{ \"result\": \"success\"}";
+        }
+    }
+}
